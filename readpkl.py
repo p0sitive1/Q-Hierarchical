76713c2bf47a801c94d831631a0eb200cc214783
P0sitive <andrewshi2002@163.com>
2022-11-16 00:14:59 +0800

    first commit

--- readpkl.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open("/home/rm2022/DQN_routing/DRL/dump_dragonfly/uniform/qroute/10.0.pkl", "rb") as f:
-    data = pickle.load(f)
-
-print(data)
--- save_legend.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-fig = plt.figure()
-axes = fig.subplots(nrows=1, ncols=3)
